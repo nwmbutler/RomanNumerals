dc7ef1c25455de835c58f339f95bc907486e206b
Nick Butler <nwmbutler@gmail.com>
2020-09-02 17:02:06 +0100

    initial commit

---
+++ roman_numerals.rb
@@ -0,0 +1,11 @@
+class RomanNumerals
+
+  def to_roman(num)
+    if num === 1
+      'I'
+    elsif num === 5
+      'V'
+    end
+  end
+  
+end
---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,15 @@
+require './roman_numerals.rb'
+
+describe RomanNumerals do
+
+    let(:roman) { RomanNumerals.new }
+
+    it "can convert a roman numeral to and from an integer" do
+        expect(roman.to_roman(1)).to eq('I')
+    end  
+
+    it "can convert a roman numeral to and from an integer" do
+        expect(roman.to_roman(5)).to eq('V')
+    end  
+             
+end
