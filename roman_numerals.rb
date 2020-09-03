76307b15288a62391be961879eda0292c6aa056c
Nick Butler <nwmbutler@gmail.com>
2020-09-03 10:41:13 +0100

    added hash of roman numbers

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,5 +1,21 @@
 class RomanNumerals
 
+  ROMAN_NUMBERS = {
+    1000 => "M",  
+     900 => "CM",  
+     500 => "D",  
+     400 => "CD",
+     100 => "C",  
+      90 => "XC",  
+      50 => "L",  
+      40 => "XL",  
+      10 => "X",  
+        9 => "IX",  
+        5 => "V",  
+        4 => "IV",  
+        1 => "I",  
+  }
+
   def to_roman(num)
     rom = Array.new(num, "I")
     case rom.length
